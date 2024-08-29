cfd9f474a95c6250a218e6cfaa9ab2ad48dc1573
TheMissingOne <167905145+TheMissingDev@users.noreply.github.com>
2024-08-29 09:24:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ const bg = document.querySelector(".bg");
 let counter = 0;
 menubtn.addEventListener("click", function (e) {
   counter += 1;
-  console.log("clicked!");
-  console.log(counter);
   if (counter == 1) {
     menubtn.innerHTML =
-      "<img src='/images/icon-close-menu.svg' alt='menubar.close'>";
+      "<img src='./images/icon-close-menu.svg' alt='menubar.close'>";
       bg.style.opacity = 1;
     nav.style.opacity = 1;
     lr.style.opacity = 1;
   } else if (counter == 2) {
-    menubtn.innerHTML = "<img src='/images/icon-menu.svg' alt='menubar'>";
+    menubtn.innerHTML = "<img src='./images/icon-menu.svg' alt='menubar'>";
     bg.style.opacity = 0;
     nav.style.opacity = 0;
     lr.style.opacity = 0;
